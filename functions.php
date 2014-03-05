9d5b03e0b8ef15d22e64813d11e1d18360f964cc
Eddie Moya <eddie.moya@gmail.com>
2014-03-05 16:25:27 -0600

    adding sidebar registration for footer, and registering footer nav menu location

--- functions.php
+++ functions.php
@@ -7,3 +7,69 @@ function add_search_box_to_menu( $items, $args ) {
 
     return $items;
 }
+
+
+
+/**
+ * Register sidebars.
+ *
+ * Registers our main widget area and the front page widget areas.
+ *
+ * @since Twenty Twelve 1.0
+ */
+// remove_action( 'widgets_init', 'twentytwelve_widgets_init' ,11);
+add_action( 'widgets_init', 'atru_widgets_init' );
+
+function atru_widgets_init() {
+
+	register_sidebar( array(
+		'name' => __( 'Footer Widget Area', 'twentytwelve' ),
+		'id' => 'footer',
+		'description' => __( 'Appears on at the bottom of all pages', 'twentytwelve' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+
+
+
+remove_action( 'after_setup_theme', 'twentytwelve_setup' );
+add_action( 'after_setup_theme', 'atru_setup' );
+function atru_setup() {
+	/*
+	 * Makes Twenty Twelve available for translation.
+	 *
+	 * Translations can be added to the /languages/ directory.
+	 * If you're building a theme based on Twenty Twelve, use a find and replace
+	 * to change 'twentytwelve' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'twentytwelve', get_template_directory() . '/languages' );
+
+	// This theme styles the visual editor with editor-style.css to match the theme style.
+	add_editor_style();
+
+	// Adds RSS feed links to <head> for posts and comments.
+	add_theme_support( 'automatic-feed-links' );
+
+	// This theme supports a variety of post formats.
+	add_theme_support( 'post-formats', array( 'aside', 'image', 'link', 'quote', 'status' ) );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menu( 'primary', __( 'Header Navigation Menu', 'twentytwelve' ) );
+		// This theme uses wp_nav_menu() in one location.
+	register_nav_menu( 'footer', __( 'Footer Navigation Menu', 'twentytwelve' ) );
+
+	/*
+	 * This theme supports custom background color and image,
+	 * and here we also set up the default background color.
+	 */
+	add_theme_support( 'custom-background', array(
+		'default-color' => 'e6e6e6',
+	) );
+
+	// This theme uses a custom image size for featured images, displayed on "standard" posts.
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 624, 9999 ); // Unlimited height, soft crop
+}
