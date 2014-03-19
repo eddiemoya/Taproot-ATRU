5cbc53865d10d524907e94f9d00544e61914bf60
Eddie Moya <eddie.moya@gmail.com>
2014-03-19 10:35:09 -0500

    removing comments and pagination links from page template closes #21

--- content-page.php
+++ content-page.php
@@ -8,8 +8,6 @@
  */
 ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
