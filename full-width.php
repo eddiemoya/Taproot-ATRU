5d0c1a523db721ce8dc5e25eb8e641345f9dc566
Eddie Moya <eddie.moya@gmail.com>
2014-09-04 17:42:35 -0500

    adding breadcrumbs to full-width page-template

---
+++ full-width.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Full-width Page Template, BreadCrumbs, No Sidebar
+ *
+ * Description: Twenty Twelve loves the no-sidebar look as much as
+ * you do. Use this page template to remove the sidebar from any page.
+ *
+ * Tip: to remove the sidebar from all posts and pages simply remove
+ * any active widgets from the Main Sidebar area, and the sidebar will
+ * disappear everywhere.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<div class="breadcrumbs"><?php if(function_exists('bcn_display')){ bcn_display();}?></div>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
