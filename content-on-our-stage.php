6ea6eb31fd4662d870d0cf8f8248b911cc3184ab
Eddie Moya <eddie.moya@gmail.com>
2014-09-04 17:19:57 -0500

    fixing artist and performance on on our stage - also alignment - addresses #142 & #110

--- content-on-our-stage.php
+++ content-on-our-stage.php
@@ -1,10 +1,12 @@
 	<?php $metadata = get_post_custom(); ?>
+	<?php $artist = array_pop(wp_get_post_terms(get_the_ID(), 'theatre_artists')); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if ( has_post_thumbnail()) : ?>
 	 		<?php the_post_thumbnail('medium'); ?>
 		<?php endif; ?>
 		<header class="entry-header">
-			<h2 class="entry-title"><?php the_title(); ?></h2>
+			<h2 class="artist-name"><?php echo $artist->name; ?></h2>
+			<h3 class="entry-title"><?php the_title(); ?></h3>
 		</header>
 		<section class="button-list">
 			<a class="learn-more button" href="<?php the_permalink(); ?>" alt="Learn more about <?php the_title(); ?>">Learn More</a>
