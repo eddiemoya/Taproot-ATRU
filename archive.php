d4b6740d16b4bd32efddc6d9f459b610d3db0711
Jon Cook <cookj@klearsky.com>
2014-11-04 15:42:40 -0500

    Updates to Genre list pages for sorting by date

--- archive.php
+++ archive.php
@@ -13,11 +13,41 @@ get_header(); ?>
 		<div id="content" role="main">
 			
 			<div class="breadcrumbs"><?php if(function_exists('bcn_display')){ bcn_display();}?></div>
-
-			<?php if(have_posts()) : ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
+			<?php 
+			
+			$pList = new WP_Query(array(
+				'post_type' => get_post_type( $post ),  
+				'meta_query' => array(
+					array(
+						'key' => 'tcms_closing',
+						'compare' => '>=',
+						'value' => date('Y-m-d', strtotime('now')),
+					)
+				),
+				'meta_key' => 'tcms_opening',
+				'orderby' => 'meta_value_num', 
+				'order' => 'ASC',
+				'posts_per_page' => -1
+				)
+			); 
+			$metakeys = get_post_meta($post->ID);
+			$genreMain =  get_the_term_list( $metakeys['tcms_productionID'][0], 'genre', '', ', ', ' ' );
+			
+			//PRODUCTION WITH GENRE
+			if ($genreMain != ""){?>
+			<?php if($pList->have_posts()) : ?>
+			<?php //print_r(get_post_meta($post->ID)); ?>
+<?php //echo get_post_type( $post ); echo get_term($post, 'genre'); ?>
+				<?php while ( $pList->have_posts() ) : $pList->the_post(); ?>
+					<?php 
+					$metakey = get_post_meta($post->ID); 
+					$genre = get_the_term_list( $metakeys['tcms_productionID'][0], 'genre', '', ', ', ' ' );
+					if($genreMain == $genre){
+					?>
 					<?php get_template_part( 'excerpt', $post->post_type ); ?>
+					<?php 
+					}
+					 ?>
 				<?php endwhile; ?>
 
 			<?php else: ?>
@@ -26,7 +56,30 @@ get_header(); ?>
 				<?php get_template_part( 'content', $post->post_type); ?>
 
 			<?php endif; // end of the loop. ?>
-			
+			<?php }
+			//NOT A PRODUCTION WITH GENRE
+			else
+			{
+				
+				?>
+			<?php if(have_posts()) : ?>
+			<?php //print_r(get_post_meta($post->ID)); ?>
+<?php //echo get_post_type( $post ); echo get_term($post, 'genre'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					
+					<?php get_template_part( 'excerpt', $post->post_type ); ?>
+					
+				<?php endwhile; ?>
+
+			<?php else: ?>
+
+				<?php wp_reset_query(); ?>
+				<?php get_template_part( 'content', $post->post_type); ?>
+
+			<?php endif; // end of the loop. ?>
+			<?php
+			}
+			?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 	
